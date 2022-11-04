33a24a9aac93867d643f006a56d9b4d14667e5cd
Justin <justakinyi@gmail.com>
2022-11-04 12:26:06 +0300

    post request

--- task/__init__.py
+++ task/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, request,render_template
+from flask import Flask, request
 from dotenv import load_dotenv
 import os
 from enum import Enum
@@ -32,21 +32,18 @@ def calculation():
             SUB = 'subtract'
             MULT = 'multiply'
         operations = [item.value for item in calc]
-        try:
-            if operator in operations:
-                if operator == 'add':
-                    result = value_x + value_y
-                if operator == 'subtract':
-                    result = value_x - value_y
-                if operator == 'multiply':
-                    result = value_x * value_y
-            print(result)
-        except:
-            print('Use add, subract or multiply for operation type')
         myjson={
-                'slackUsername': os.getenv('SLACK_USERNAME'),
-                'operation_type': operator,
-                'result': result
-            }
-        return(myjson)
-    return('your request was successfull just nothing to render')
+                    'slackUsername': os.getenv('SLACK_USERNAME'),
+                    'operation_type': operator,
+                    'result': result
+                    }
+        if operator in operations:
+            if operator == 'add':
+                result = value_x + value_y
+                return myjson
+            if operator == 'subtract':
+                result = value_x - value_y
+                return myjson
+            if operator == 'multiply':
+                result = value_x * value_y
+                return myjson
