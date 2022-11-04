f3c8f207e1d342792a20ccd3f69db8ecbe25b9d4
Justin <justakinyi@gmail.com>
2022-11-04 13:10:23 +0300

    post request

--- task/__init__.py
+++ task/__init__.py
@@ -23,31 +23,28 @@ def index():
 @app.route('/json-example', methods=['GET','POST'])
 def calculation():
     if request.method == 'POST':
-        new_data = request.get_json(force=False,silent=False,cache=True)
+        # new_data = request.get_json(force=False,silent=False,cache=True)
         value_x = request.json['x']
         value_y = request.json['y']
         operator = request.json['operation_type']
-        print(value_x)
-        print(value_y)
-        print(operator)
-    #     class calc(Enum):
-    #         ADD = 'add'
-    #         SUB = 'subtract'
-    #         MULT = 'multiply'
-    #     operations = [item.value for item in calc]
-    #     myjson={
-    #                 'slackUsername': os.getenv('SLACK_USERNAME'),
-    #                 'operation_type': operator,
-    #                 'result': result
-    #                 }
-    #     if operator in operations:
-    #         if operator == 'add':
-    #             result = value_x + value_y
-    #             return myjson
-    #         if operator == 'subtract':
-    #             result = value_x - value_y
-    #             return myjson
-    #         if operator == 'multiply':
-    #             result = value_x * value_y
-    #             return myjson
+        class calc(Enum):
+            ADD = 'add'
+            SUB = 'subtract'
+            MULT = 'multiply'
+        operations = [item.value for item in calc]
+        myjson={
+                    'slackUsername': os.getenv('SLACK_USERNAME'),
+                    'operation_type': operator,
+                    'result': result
+                    }
+        if operator in operations:
+            if operator == 'add':
+                result = value_x + value_y
+                return myjson
+            if operator == 'subtract':
+                result = value_x - value_y
+                return myjson
+            if operator == 'multiply':
+                result = value_x * value_y
+                return myjson
     return 'myjson'
